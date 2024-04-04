1360152f0910722329c5a690b919e679ecf8dcef
tatanoi <ta.phasin@gmail.com>
2024-04-04 18:41:11 +0700

    feat : state

--- myftp.py
+++ myftp.py
@@ -1,41 +1,55 @@
 import socket
 import random
 
+isconnect = False
 
 while True:
     line = input('ftp> ').strip()
     args = line.split()
     command = args[0]
 
-    if (command == 'quit' or command == 'bye'):
-        clientSocket.send("QUIT\r\n".encode())
-        resp = clientSocket.recv(1024)
-        print(resp.decode(), end = "")
-        clientSocket.close()
-        break
-
-    elif command == 'open':
+    if command == 'open':
         clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        clientSocket.connect(("127.0.0.1", 21))
+        print(f"Connected to {args[1]}")
+        clientSocket.connect((f"{args[1]}", 21))
         resp = clientSocket.recv(1024)
         print(resp.decode(), end = "")
 
-        # user = input(f"User ({args[1]}:(none)): ")
-        # print(user)
-        clientSocket.send("USER bob\r\n".encode())
+        print("202 UTF8 mode is always enabled. No need to send this command")
+
+        user = input(f"User ({args[1]}:(none)): ")
+        clientSocket.send(f"USER {user}\r\n".encode())
         resp = clientSocket.recv(1024)
         print(resp.decode(), end = "")
 
-        # password = input("Password:")
-        clientSocket.send("PASS 12345\r\n".encode())
+        password = input("Password:")
+        clientSocket.send(f"PASS {password}\r\n".encode())
         resp = clientSocket.recv(1024)
         print(resp.decode(), end = "")
+        isconnect = True
+
+    if (isconnect == False and command in ['ascii','binary','cd','close','delete','disconnect','get','ls','open','put','pwd','rename','user']):
+        print("Not connected.")
+
+    elif (command == 'quit' or command == 'bye'):
+        try:
+            clientSocket.send("QUIT\r\n".encode())
+            resp = clientSocket.recv(1024)
+            print(resp.decode(), end = "")
+            clientSocket.close()
+            isconnect = False
+            break
+        except:
+            break
 
     elif (command == 'disconnect' or command == 'close'):
         clientSocket.send("QUIT\r\n".encode())
         resp = clientSocket.recv(1024)
         print(resp.decode(), end = "")
-        clientSocket.close()    
+        clientSocket.close()  
+        isconnect = False  
+    
+
 
 
     elif command == 'ls':
